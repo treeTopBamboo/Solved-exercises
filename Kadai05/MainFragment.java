3f0899b761a73f9b0b4ec955ffd9adeaa82c0fc8
treeTopBamboo <74302467+treeTopBamboo@users.noreply.github.com>
2020-12-03 17:51:18 +0900

    Add files via upload

--- Kadai05/MainFragment.java
+++ Kadai05/MainFragment.java
@@ -1,14 +1,21 @@
 package org.example.kadai05;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,12 +23,15 @@ import androidx.fragment.app.FragmentManager;
 import java.util.Objects;
 
 public class MainFragment extends Fragment {
+    TextView textView;
     //スマホとタブレットの判定用
     private boolean isTwoPane = false;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Activity activity = Objects.requireNonNull(getActivity());
+        //subFrameの有無で利用レイアウトの判定
         if(activity.findViewById(R.id.subFrame) != null) {
             isTwoPane = true;
         }
@@ -41,42 +51,65 @@ public class MainFragment extends Fragment {
         //レイアウトファイルからViewオブジェクト生成
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        //Blueクリックで青色設定用の変数
-        Button btnBlue = view.findViewById(R.id.btn_setBlue);
-        btnBlue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                color_id = Color.BLUE;
-            }
-        });
-        //Defaultクリックでデフォルト設定用の変数
-        Button btnDefault = view.findViewById(R.id.btn_setDefault);
-        btnDefault.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                color_id = DEFAULT_COLOR;
-            }
-        });
+        try{
+            //Blueクリックで青色設定用の変数
+            Button btnBlue = view.findViewById(R.id.btn_setBlue);
+            btnBlue.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    color_id = Color.BLUE;
+                    //青色ボタンでのフラグメント起動準備
+                    FragmentManager manager = Objects.requireNonNull(getFragmentManager());
+                    SubFragment fragment = new SubFragment();
+                    //フラグメントに青色用の値を設定
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("color", color_id);
+                    //タブレットならFragment
+                    if (isTwoPane) {
+                        fragment.setArguments(bundle);
+                        manager.beginTransaction()
+                                .replace(R.id.subFrame, fragment)
+                                .commit();
+                    } else {
+                        //スマホならFragment情報ををActivityに
+                        Intent intent = new Intent(getActivity(), SubActivity.class);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                }
+            });
+            //Defaultクリックでデフォルト設定用の変数
+            Button btnDefault = view.findViewById(R.id.btn_setDefault);
+            btnDefault.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    color_id = DEFAULT_COLOR;
+                    //デフォルトボタンでのフラグメント起動準備
+                    FragmentManager manager = Objects.requireNonNull(getFragmentManager());
+                    SubFragment fragment = new SubFragment();
+                    //フラグメントにデフォルト用値を設定
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("color", color_id);
+                    //タブレットならFragmentで
+                    if (isTwoPane) {
+                        fragment.setArguments(bundle);
+                        manager.beginTransaction()
+                                .replace(R.id.subFrame, fragment)
+                                .commit();
+                    } else {
+                        //スマホならFragment情報ををActivityに
+                        Intent intent = new Intent(getActivity(), SubActivity.class);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                }
+            });
 
-        //フラグメント起動準備
-        FragmentManager manager = Objects.requireNonNull(getFragmentManager());
-        SubFragment fragment = new SubFragment();
-        //フラグメントに値を設定
-        Bundle bundle = new Bundle();
-        bundle.putInt("color", color_id);
-        //タブレットならFragment
-        if (isTwoPane) {
-            fragment.setArguments(bundle);
-            manager.beginTransaction()
-                    .replace(R.id.subFrame, fragment)
-                    .commit();
-        } else {
-            //スマホならFragment情報ををActivityに
-            Intent intent = new Intent(getActivity(), SubActivity.class);
-            intent.putExtras(bundle);
-            startActivity(intent);
+        }catch (Exception e){
+            //エラーした時用の表示
+            textView.findViewById(R.id.textView);
+            textView.setText(String.format("Fragment Error"));
         }
-
         return view;
     }
 
--- Kadai05/SubFragment.java
+++ Kadai05/SubFragment.java
@@ -1,9 +1,11 @@
 package org.example.kadai05;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +35,7 @@ public class SubFragment extends Fragment {
     final private static int DEFAULT_COLOR = -1;
     static int color_id = DEFAULT_COLOR;
 
+    @SuppressLint("ResourceType")
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater,
@@ -41,23 +44,14 @@ public class SubFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_sub, container, false);
         Bundle bundle;
         final Activity activity = Objects.requireNonNull(getActivity());
-        //タブレットの場合
+        //タブレットの場合の表示
         if(isTwoPane) {
             bundle = Objects.requireNonNull(getArguments());
         } else {
-            //スマホ環境の場合
+            //スマホ環境の場合の表示
             Intent intent = activity.getIntent();
             bundle = Objects.requireNonNull(intent.getExtras());
         }
-
-        //閉じるボタン
-        Button btnClose = view.findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().remove(SubFragment.this).commit();
-            }
-        });
 
         //受け取った色変数に応じて色変更
         TableLayout tl = view.findViewById(R.id.tl_tableLayout);
@@ -66,16 +60,31 @@ public class SubFragment extends Fragment {
         switch (id) {
             case Color.BLUE:
                 tl.setBackgroundColor(Color.BLUE);
-                txtColor.setText("Blue");
+                txtColor.setText("blue");
                 break;
             case DEFAULT_COLOR:
-                tl.setBackgroundResource(DEFAULT_COLOR);
-                txtColor.setText("Default");
+                tl.setBackgroundColor(Color.WHITE);
+                txtColor.setText("default");
                 break;
             default:
                 // fail safe
                 break;
         }
+        //閉じるボタン
+        Button btnClose = view.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTwoPane){
+                    //タブレット用の閉じる動作
+                    getFragmentManager().beginTransaction().remove(SubFragment.this).commit();
+                }else{
+                    //スマホ用の閉じる動作
+                    activity.finish();
+                }
+            }
+        });
+
         return view;
     }
 
